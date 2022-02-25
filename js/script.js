8919de039cd360f2c0e345fe430721059a1eee30
César Augusto <ces.augusto108@gmail.com>
2022-02-25 19:56:53 -0300

    Sequência de acertos, manutenção de vida a cada acerto, perda a cada erro, recuperação de vida com sequência de 10.

--- js/script.js
+++ js/script.js
@@ -1,59 +1,82 @@
 "use strict";
 
 const target = document.getElementById("target");
+let count = 0;
 
 const gameStats = {
-	score: 0,
-	lives: 5,
-	points: 0,
-	sequence: 0,
+      score: 0,
+      lives: 5,
+      onSequence: false,
+      shotBirdsInSequence: 0,
 };
 
 const showTarget = function () {
-	const size = () => Math.trunc((Math.random() * 6 + 4) * 10);
-	const positionY = () => Math.trunc(Math.random() * 50 + 1);
-	const positionX = () => {
-		if (window.visualViewport.width < 460) return Math.trunc(Math.random() * 70 + 1);
-		else return Math.trunc(Math.random() * 90 + 1);
-	};
+      const size = () => Math.trunc((Math.random() * 6 + 4) * 10);
+      const positionY = () => Math.trunc(Math.random() * 50 + 1);
+      const positionX = () => {
+            if (window.visualViewport.width < 460) return Math.trunc(Math.random() * 70 + 1);
+            else return Math.trunc(Math.random() * 90 + 1);
+      };
 
-	target.style.display = "block";
-	target.style.mixBlendMode = "normal";
-	target.style.height = `${size()}px`;
-	target.style.left = `${positionX()}vw`;
-	target.style.top = `${positionY()}vh`;
+      target.style.display = "block";
+      target.style.mixBlendMode = "normal";
+      target.style.height = `${size()}px`;
+      target.style.left = `${positionX()}vw`;
+      target.style.top = `${positionY()}vh`;
 
-	const rotate = () => {
-		switch (Math.trunc(Math.random() * 6)) {
-			case 0:
-				target.style.transform = "rotateY(0deg) rotateZ(0deg)";
-				break;
-			case 1:
-				target.style.transform = "rotateY(0deg) rotateZ(15deg)";
-				break;
-			case 2:
-				target.style.transform = "rotateY(0deg) rotateZ(345deg)";
-				break;
-			case 3:
-				target.style.transform = "rotateY(180deg) rotateZ(0deg)";
-				break;
-			case 4:
-				target.style.transform = "rotateY(180deg) rotateZ(15deg)";
-				break;
-			case 5:
-				target.style.transform = "rotateY(180deg) rotateZ(345deg)";
-				break;
-		}
-	};
-	rotate();
+      const rotate = () => {
+            switch (Math.trunc(Math.random() * 6)) {
+                  case 0:
+                        target.style.transform = "rotateY(0deg) rotateZ(0deg)";
+                        break;
+                  case 1:
+                        target.style.transform = "rotateY(0deg) rotateZ(15deg)";
+                        break;
+                  case 2:
+                        target.style.transform = "rotateY(0deg) rotateZ(345deg)";
+                        break;
+                  case 3:
+                        target.style.transform = "rotateY(180deg) rotateZ(0deg)";
+                        break;
+                  case 4:
+                        target.style.transform = "rotateY(180deg) rotateZ(15deg)";
+                        break;
+                  case 5:
+                        target.style.transform = "rotateY(180deg) rotateZ(345deg)";
+                        break;
+            }
+      };
+      rotate();
 };
 
 const eliminateTarget = function () {
-	target.style.mixBlendMode = "hue";
+      target.style.mixBlendMode = "screen";
+      gameStats.score += 10;
+      gameStats.lives += 1;
+      gameStats.onSequence = true;
 };
 
-setInterval(() => {
-	showTarget();
-}, 1600);
+function gameRun() {
+      setInterval(() => {
+            showTarget();
+            target.addEventListener("click", eliminateTarget);
 
-target.addEventListener("click", eliminateTarget);
+            if (gameStats.onSequence !== false) {
+                  count += 1;
+                  gameStats.shotBirdsInSequence = count;
+            } else {
+                  gameStats.shotBirdsInSequence = 0;
+                  count = 0;
+            }
+
+            if (gameStats.lives < 5 && gameStats.shotBirdsInSequence !== 0 && gameStats.shotBirdsInSequence % 10 === 0) gameStats.lives += 1;
+
+            gameStats.onSequence = false;
+
+            console.log("score " + gameStats.score, "shotBirdsInSequence " + gameStats.shotBirdsInSequence, "lives " + gameStats.lives);
+            
+            gameStats.lives -= 1;
+      }, 1600);
+}
+
+gameRun();
